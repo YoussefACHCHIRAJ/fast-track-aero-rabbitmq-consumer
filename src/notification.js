b928f371d0d5ace06bd43f96a26934381b92c9cd
Youssef ACHCHIRAJ <youssefachchiraj@gmail.com>
2025-02-24 11:09:34 +0000

    add ttl option

--- src/notification.js
+++ src/notification.js
@@ -1,5 +1,9 @@
 const admin = require("../config/firebase");
 
+function toUnixTimestamp(ttlInSeconds) {
+  return Math.floor(Date.now() / 1000) + ttlInSeconds;
+}
+
 async function sendPushNotification(payload) {
   if (!payload.target || !payload.notification) {
     throw new Error("Invalid notification structure");
@@ -10,13 +14,16 @@ async function sendPushNotification(payload) {
       title: payload.notification.title,
       body: payload.notification.body,
     },
-    data: {
-      title: payload.notification.title,
-      body: payload.notification.body,
-    },
     topic: payload.target.value,
   };
 
+  if (payload.notification.ttl) {
+    androidTtl = payload.notification.ttl;
+    iosTtl = toUnixTimestamp(payload.notification.ttl).toString();
+
+    message.android.ttl = androidTtl;
+    message.apns.headers["apns-expiration"] = iosTtl;
+  }
   await admin.messaging().send(message);
   console.log("✅ Push notification sent successfully!");
 }

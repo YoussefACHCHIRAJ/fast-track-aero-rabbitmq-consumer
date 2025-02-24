eef189387eaea46533de0c35eec650982df106ae
Youssef ACHCHIRAJ <youssefachchiraj@gmail.com>
2025-02-24 11:22:19 +0000

    fixing syntax error

--- src/notification.js
+++ src/notification.js
@@ -18,14 +18,24 @@ async function sendPushNotification(payload) {
   };
 
   if (payload.notification.ttl) {
-    androidTtl = payload.notification.ttl;
-    iosTtl = toUnixTimestamp(payload.notification.ttl).toString();
+    const androidTtl = payload.notification.ttl;
 
-    message.android.ttl = androidTtl;
-    message.apns.headers["apns-expiration"] = iosTtl;
+    const iosTtl = toUnixTimestamp(payload.notification.ttl).toString();
+
+    message.android = { ttl: androidTtl };
+    message.apns = {
+      headers: {
+        "apns-expiration": iosTtl,
+      },
+    };
   }
-  await admin.messaging().send(message);
-  console.log("✅ Push notification sent successfully!");
+  try {
+    await admin.messaging().send(message);
+    console.log("✅ Push notification sent successfully!");
+  } catch (error) {
+    console.error("❌ Failed to send push notification:", error);
+    throw new Error("Failed to send push notification");
+  }
 }
 
 module.exports = sendPushNotification;
